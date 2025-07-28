37111b21cf793ade88b1590259c6a0d423925c63
editscape1 <editscape.org@gmail.com>
2025-07-28 15:35:45 +0530

    Update Portfolio.tsx

--- frontend/src/components/Portfolio.tsx
+++ frontend/src/components/Portfolio.tsx
@@ -125,7 +125,7 @@ export const Portfolio = () => {
     type="video/mp4"
     controls
     preload="metadata"
-    className="w-full h-60% object-contain cursor-pointer"
+    className="w-full h-50% object-contain cursor-pointer"
     onClick={(e) => {
       if (e.target.paused) {
         e.target.play();
@@ -141,7 +141,7 @@ export const Portfolio = () => {
 <img
   src={item.image_url}
   alt={item.title}
-  className="w-full h-60% object-contain cursor-pointer"
+  className="w-full h-50% object-contain cursor-pointer"
   onClick={(e) => {
     if (e.target.requestFullscreen) {
       e.target.requestFullscreen();
@@ -156,7 +156,7 @@ export const Portfolio = () => {
                     </div>
                     <div className="p-4">
                       <h3 className="text-lg font-bold text-white mb-2 tracking-wider">{item.title}</h3>
-                      <p className="text-gray-400 text-[12px] leading-tight">
+                      <p className="text-gray-350 text-[13px] leading-tight">
                         {item.description}
                       </p>
                     </div>
